a8132f34dba813583d8c38e98db2997f2e3a805e
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-11-04 10:07:53 +0200

    add try...catch

--- contacts.js
+++ contacts.js
@@ -7,38 +7,54 @@ const contactsPath = path.resolve('db', 'contacts.json');
 const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 async function listContacts() {
-  const result = await fs.readFile(contactsPath, 'utf-8');
-  return JSON.parse(result);
+  try {
+    const result = await fs.readFile(contactsPath, 'utf-8');
+    return JSON.parse(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getContactById(id) {
-  const contacts = await listContacts();
-  const result = contacts.find(contact => contact.id === id);
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find(contact => contact.id === id);
+    return result || null;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function addContact({ name, email, phone }) {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await updateContacts(contacts);
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function removeContact(id) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(contact => contact.id === id);
-  if (index === -1) {
-    return null;
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+    return result;
+  } catch (error) {
+    console.log(error.message);
   }
-  const [result] = contacts.splice(index, 1);
-  await updateContacts(contacts);
-  return result;
 }
 
 export { listContacts, getContactById, addContact, removeContact };
