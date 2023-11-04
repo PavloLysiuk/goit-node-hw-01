f122dfa85dfd43f47dcd5501393fa9d3539a1349
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-11-04 03:07:21 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,44 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+async function listContacts() {
+  const result = await fs.readFile(contactsPath, 'utf-8');
+  return JSON.parse(result);
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === id);
+  return result || null;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+export { listContacts, getContactById, addContact, removeContact };
